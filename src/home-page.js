81633b701f17ccd0b1c80e436ebc01d69b98e212
adam <19adamchristie96@gmail.com>
2023-08-06 02:03:58 +0100

    Create homepage function which creates a basic homepage layout with elements

---
+++ src/home-page.js
@@ -0,0 +1,25 @@
+import homePageImage from './assets/home-page-image.jpeg'
+
+function renderHomePage(){
+    const contentContainer = document.querySelector('#content');
+    
+    //Create title for the page
+    const titleElement = document.createElement('h1');
+    titleElement.innerText = "Big Bob's Burgers";
+    titleElement.className = "title";
+
+    //Add image to the page
+    const imageElement = document.createElement('img');
+    imageElement.src = homePageImage;
+
+    //Create text for page
+    const pageText = document.createElement('p');
+    pageText.innerText = "Home of the Belly Buster Burger! Here at BBB's, we use the freshest ingredients for your gourmet experience at an unbeatable cost!"
+    pageText.className = "page-text";
+    
+    contentContainer.appendChild(titleElement);
+    contentContainer.appendChild(imageElement);
+    contentContainer.appendChild(pageText);
+}
+
+export default renderHomePage
